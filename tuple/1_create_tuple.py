fbcbf863193135b332287b840d8024038551ee6e
Neelesh2909 <rawat.neelesh9@gmail.com>
2022-03-11 15:45:50 +0530

    [Neelesh] Add: Python program to create a tuple

---
+++ tuple/1_create_tuple.py
@@ -0,0 +1,23 @@
+'''
+
+    @Author: Neelesh Rawat
+    @Date: 10-03-2022
+    @Last Modified by: Neelesh Rawat
+    @Last Modified time: 11-03-11
+    @Title : Create a tuple
+
+'''
+
+def main():
+    #creating an empty tuple
+    t = ()
+    print(t)
+    # Create an empty tuple with tuple() function built-in
+    tuplex =  tuple()
+    print(tuplex)
+    #tuple with elements
+    x = (10,20,30,40)
+    print(x)
+
+if __name__ == "__main__":
+    main()
