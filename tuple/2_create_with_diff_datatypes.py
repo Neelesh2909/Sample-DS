6b13642a99ebec4c0f73ba90a77316043c9cac72
Neelesh2909 <rawat.neelesh9@gmail.com>
2022-03-11 15:47:41 +0530

    [Neelesh] Add: Python program to create a tuple containing different datatypes

---
+++ tuple/2_create_with_diff_datatypes.py
@@ -0,0 +1,16 @@
+'''
+
+    @Author: Neelesh Rawat
+    @Date: 10-03-2022
+    @Last Modified by: Neelesh Rawat
+    @Last Modified time: 11-03-11
+    @Title : Create a tuple with different datatypes
+
+'''
+
+def main():
+    x = (10,'Neelesh',11.5,True)
+    print(x)
+
+if __name__ == "__main__":
+    main()
